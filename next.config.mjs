cffdd8cc3e7105c4c1d423ddd46fd6176a9f3b09
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-29 19:48:14 +0530

    add pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,26 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+import WithPwa from 'next-pwa';
+await import('./src/env.mjs');
+
+const withPWA = WithPwa({
+	dest: 'public',
+});
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	/**
+	 * If you are using `appDir` then you must comment the below `i18n` config out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
 };
 
-export default config;
+export default withPWA(config);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,25 @@ import type { DocumentContext } from 'next/document';
 const MyDocument = () => (
 	<Html lang='en'>
 		<Head>
+			<meta
+				name='viewport'
+				content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+			/>
 			<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
 			<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
 			<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
 			<link rel='manifest' href='/site.webmanifest' />
+			<meta name='application-name' content='Atoms' />
+			<meta name='apple-mobile-web-app-capable' content='yes' />
+			<meta name='apple-mobile-web-app-status-bar-style' content='default' />
+			<meta name='apple-mobile-web-app-title' content='Atoms' />
+			<meta name='description' content='Portfolio Tracker with Superpowers' />
+			<meta name='format-detection' content='telephone=no' />
+			<meta name='mobile-web-app-capable' content='yes' />
+			<meta name='msapplication-config' content='/icons/browserconfig.xml' />
+			<meta name='msapplication-TileColor' content='#2B5797' />
+			<meta name='msapplication-tap-highlight' content='no' />
+			<meta name='theme-color' content='#9d72ff' />
 		</Head>
 		<body>
 			<Main />
