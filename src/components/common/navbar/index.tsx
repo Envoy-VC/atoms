32b964e8438e58410cdde8c9c11aad452f96dc3b
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-29 19:31:05 +0530

    feat/ add mobile drawer

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Input } from 'antd';
+import { Button, Input } from 'antd';
+import NavDrawer from './drawer';
 import ConnectButton from '../connect-button';
 // Icons
 import { TbSearch, TbSlash, TbBleachNoChlorine } from 'react-icons/tb';
@@ -8,8 +9,9 @@ import { TbSearch, TbSlash, TbBleachNoChlorine } from 'react-icons/tb';
 import { PiListBold } from 'react-icons/pi';
 
 const Navbar = () => {
+	const [navDrawerOpen, setNavDrawerOpen] = React.useState<boolean>(false);
 	return (
-		<div className='w-full p-3 sm:p-6'>
+		<div className='px-0 py-3 sm:p-6'>
 			<div className='border-grayscale hidden flex-row items-center justify-between rounded-xl border-[1px] bg-white p-2 px-4 sm:flex '>
 				<Input
 					placeholder='Search'
@@ -24,16 +26,23 @@ const Navbar = () => {
 				/>
 				<ConnectButton />
 			</div>
+
 			<div className='flex flex-row items-center justify-between sm:hidden'>
 				<div className='flex flex-row items-center gap-2'>
-					<PiListBold size={28} className='text-slate-800' />
+					<Button
+						icon={<PiListBold size={28} className='text-slate-800' />}
+						type='link'
+						onClick={() => setNavDrawerOpen((prev) => !prev)}
+					/>
+
 					<div className='flex flex-row items-center gap-1'>
-						<TbBleachNoChlorine size={36} className='text-primary' />
+						<TbBleachNoChlorine size={36} className='text-primary mt-1' />
 						<div className='navTitle text-2xl font-medium'>atoms</div>
 					</div>
 				</div>
 				<ConnectButton />
 			</div>
+			<NavDrawer isOpen={navDrawerOpen} setOpen={setNavDrawerOpen} />
 		</div>
 	);
 };
